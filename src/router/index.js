cef52701f6596fd6788c6365c1d870c6d1f2e71b
sun <1874281901@qq.com>
2020-11-29 17:15:54 +0800

    change demo1

---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+const Home = () => import('@/views/Home.vue')
+const Login = () => import('@/views/Login.vue')
+const Register = () => import('@/views/Register.vue')
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    { path: '/', redirect: "/home" },
+    { path: '/home', component: Home },
+    { path: '/login', component: Login },
+    { path: '/register', component: Register },
+  ]
+});
+export default router
